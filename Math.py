7554ab56833b47f6c360aa222c173a49dfee1d63
JamesJobrien <103634489+JamesJobrien@users.noreply.github.com>
2022-04-14 10:57:27 +0100

    Create Math.py

---
+++ Math.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+#Math
+print (50 + 50)#add
+print (50 - 50)#subtract
+print (50 * 50)#multiply
+print (50 / 50)#divide
+print (50 + 50 - 50 * 50 / 50)#PEMDAS
+print (50 **2)#expontents to the power of
+print (50 %6)#modulo shows the remander
+print (50 / 6)#shows you the actual divided share
+print (50 // 6)#no leftovers
