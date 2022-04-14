b5861f78f28094a6809b719e0b208810ff6a2489
JamesJobrien <103634489+JamesJobrien@users.noreply.github.com>
2022-04-14 11:03:23 +0100

    Create Varibles.py

---
+++ Varibles.py
@@ -0,0 +1,7 @@
+#Varibles methods
+quote = "All is fair in love and war."
+print (quote.upper())#upper case
+print (quote.lower())#lowercase
+print(quote.title())#titlecase
+
+print (len(quote)) #length including spaces
